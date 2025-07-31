e13596f6c2fc8439cc74301f6b742b1877cb29d0
VinniSpi <vinnibick@gmail.com>
2025-07-31 14:55:08 +0300

    feat: modify flag util for parsing arguments

--- internal/utils/flag.go
+++ internal/utils/flag.go
@@ -6,13 +6,19 @@ import (
 	"flag"
 )
 
-func FlagParse() (*models.Args, error) {
-	flag.Parse()
+func ArgParse() (*models.Args, error) {
 	var args models.Args
 
 	flag.Float64Var(&args.Amount, "amount", 0, "Amount of currency")
 	flag.StringVar(&args.From, "from", "", "From currency")
 	flag.StringVar(&args.To, "to", "", "To currency")
+	flag.BoolVar(&args.List, "list", false, "List all available currencies")
+
+	flag.Parse()
+
+	if args.List {
+		return &args, nil
+	}
 
 	if args.Amount == 0 {
 		return nil, errors.New("zero value of currency")
