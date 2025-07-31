f47f75f0f751ceb8b5c17b91a614a7617d165a18
VinniSpi <vinnibick@gmail.com>
2025-07-31 15:51:54 +0300

    feat: add storage module for file actions

---
+++ internal/storage/storage.go
@@ -0,0 +1,37 @@
+package storage
+
+import (
+	"fmt"
+	"os"
+)
+
+func OpenFile(filePath string) (*os.File, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			file, err = os.Create(filePath)
+			if err != nil {
+				return nil, fmt.Errorf("failed to create file %s: %w", filePath, err)
+			}
+			return file, nil
+		}
+		return nil, fmt.Errorf("failed to open file %s: %w", filePath, err)
+	}
+	return file, nil
+}
+
+func MarshalFile(file *os.File) ([]byte, error) {
+	data, err := os.ReadFile(file.Name())
+	if err != nil {
+		return nil, fmt.Errorf("failed to read file %s: %w", file.Name(), err)
+	}
+	return data, nil
+}
+
+func SaveFile(filePath string, data []byte) error {
+	err := os.WriteFile(filePath, data, 0644)
+	if err != nil {
+		return fmt.Errorf("failed to write to file %s: %w", filePath, err)
+	}
+	return nil
+}
