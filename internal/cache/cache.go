9cc19710bb5f48df4119d1feac42d0dd0bb66731
VinniSpi <vinnibick@gmail.com>
2025-07-31 16:28:02 +0300

    feat: throw errors in cache

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -11,46 +11,53 @@ func GetCacheKey(args *models.Args) string {
 	return args.From + "_" + args.To
 }
 
-func GetCacheValue(cachePath, key string) *float64 {
-	cache := getCache(cachePath)
+func GetCacheValue(cachePath, key string) (*float64, error) {
+	cache, err := getCache(cachePath)
+	if err != nil {
+		return nil, err
+	}
 	if cache == nil {
-		return nil
+		return nil, nil
 	}
 	if data, exists := (*cache)[key]; exists {
 		if data.ExpirationDate < time.Now().Format(time.RFC3339) {
 			delete((*cache), key)
 			updatedData, err := json.Marshal(cache)
 			if err != nil {
-				storage.SaveFile(cachePath, updatedData)
+				err = storage.SaveFile(cachePath, updatedData)
+				return nil, err
 			}
-			return nil
+			return nil, nil
 		}
-		return &data.Currency
+		return &data.Currency, nil
 	}
-	return nil
+	return nil, nil
 }
 
-func getCache(cachePath string) *map[string]models.CacheData {
+func getCache(cachePath string) (*map[string]models.CacheData, error) {
 	file, err := storage.OpenFile(cachePath)
 	if err != nil {
-		return nil
+		return nil, err
 	}
 	defer file.Close()
 
 	value, err := storage.MarshalFile(file)
 	if err != nil {
-		return nil
+		return nil, err
 	}
 	var cache map[string]models.CacheData
 	err = json.Unmarshal(value, &cache)
 	if err != nil {
-		return nil
+		return nil, err
 	}
-	return &cache
+	return &cache, nil
 }
 
-func SetCacheValue(cachePath, key string, value float64) {
-	cache := getCache(cachePath)
+func SetCacheValue(cachePath, key string, value float64) error {
+	cache, err := getCache(cachePath)
+	if err != nil {
+		return err
+	}
 	if cache == nil {
 		cache = &map[string]models.CacheData{}
 	}
@@ -60,10 +67,12 @@ func SetCacheValue(cachePath, key string, value float64) {
 	}
 	updatedData, err := json.Marshal(cache)
 	if err != nil {
-		return
+		return err
 	}
 	err = storage.SaveFile(cachePath, updatedData)
 	if err != nil {
-		return
+		return err
 	}
+
+	return nil
 }
